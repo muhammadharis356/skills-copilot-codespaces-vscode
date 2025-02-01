55a3f27d58c193300a65114ce97981cd73efaf34
Muhammad Haris <muhammadharisjappa@gmail.com>
2025-02-01 10:15:19 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,37 +1,37 @@
-//create web server
+// Create web server
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
 const url = require('url')
 
-//create web server
+// Create web server
 const server = http.createServer((req, res) => {
-    //get the path
+    // get the path
     let pathName = url.parse(req.url, true).pathname
-    //get the method
+    // get the method
     let method = req.method
 
-    //if the path is /comments
+    // if the path is /comments
     if (pathName === '/comments') {
-        //if the method is post
+        // if the method is post
         if (method === 'POST') {
-            //read the file
+            // read the file
             fs.readFile(path.join(__dirname, 'data', 'comments.json'), 'utf-8', (err, data) => {
                 if (err) {
                     res.end('Error')
                 } else {
-                    //parse the data
+                    // parse the data
                     let comments = JSON.parse(data)
-                    //get the new comment
+                    // get the new comment
                     let body = ''
                     req.on('data', chunk => {
                         body += chunk
                     })
                     req.on('end', () => {
                         let comment = JSON.parse(body)
-                        //add the new comment
+                        // add the new comment
                         comments.push(comment)
-                        //write the new comment
+                        // write the new comment
                         fs.writeFile(path.join(__dirname, 'data', 'comments.json'), JSON.stringify(comments, null, 4), err => {
                             if (err) {
                                 res.end('Error')
@@ -48,7 +48,7 @@ const server = http.createServer((req, res) => {
     }
 })
 
-//listen to the server
+// listen to the server
 server.listen(3000, () => {
     console.log('Server is running')
 })
